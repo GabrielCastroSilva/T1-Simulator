9c018c308aee6f11ab166906299ef0fef06d4768
GabrielCastroSilva <gabriel.silva.010@acad.pucrs.br>
2021-03-20 15:44:26 -0300

    basic generator created

--- src/com/simulator/Main.java
+++ src/com/simulator/Main.java
@@ -1,7 +1,25 @@
 package com.simulator;
 
 public class Main {
+    //Values of the congruential calculus (a * x(i) + c) % m
+    public static int a = 25173;
+    public static int c = 13849;
+    public static int m = 32768;
+    public static double x = m/2;
+
+
+    public static double randomizer(){
+        x = (a * x + c) % m;
+
+        return x/m;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int count = 100; //Amount of random generated numbers
+
+        for(int i = 0 ; i < count ; i++){
+            System.out.println(randomizer());
+        }
     }
 }
