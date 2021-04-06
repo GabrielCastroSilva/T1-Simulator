1691b4ae55bcd0f5fee5792efe4f0c5eb49aa6b7
GabrielCastroSilva <gabriel.silva.010@acad.pucrs.br>
2021-04-06 10:13:11 -0300

    Beta Alpha Beta implementation

--- src/com/simulator/Main.java
+++ src/com/simulator/Main.java
@@ -3,35 +3,139 @@ package com.simulator;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 public class Main {
     //Values of the congruential calculus (a * x(i) + c) % m
-    public static int a = 25173;
-    public static int c = 13849;
-    public static int m = 32768;
-    public static double x = m/2;
-
-
-    // Method that executes the linear congruential calculation
-    public static double randomizer(){
-        x = (a * x + c) % m;
-
-        return x/m;
-    }
+    private static final int a = 25173;
+    private static final int c = 13849;
+    private static final int m = 32768;
+    private static final int count = 100000; //Amount of random generated numbers
+    private static double x; //seed
+    private static int randomCount;
 
 
     public static void main(String[] args) throws IOException {
-        int count = 1000; //Amount of random generated numbers
+        double minArrival = 1;
+        double maxArrival = 2;
 
-        // Starts the writer and printer
-        FileWriter writer = new FileWriter("result.txt");
-        PrintWriter printer = new PrintWriter(writer);
+        double minService = 3;
+        double maxService = 6;
 
-        for(int i = 0 ; i < count ; i++){
-            // Writes to a .txt file the results of the randomizer method
-            printer.print(randomizer());
+        double[] seeds = {2.0, 2.1, 2.3, 2.2, 2.4}; // Seed number used in the randomizer
+        //x = m / seeds[0];
+
+
+        int servers = 1; // How many "cashiers"
+        int capacity = 5; // Max possible amount in row, -1 is "infinite"
+        int row = 0; // How full the row is
+
+        double nextArrival = 3.0;
+        double nextExit = -1;
+
+        double globalTime = 0; // Time used for calculations
+        double lastTime = 0;
+        int losses = 0;
+
+        double[] time;
+        if (capacity == -1) {
+            time = new double[10000];
+        } else {
+            time = new double[capacity + 1];
         }
-        // closes the printer and saves the file
-        printer.close();
+
+        ArrayList<double[]> events = new ArrayList<>();  // 0 is Arrival and 1 is Exit, 1 is for time
+        events.add(new double[]{0, 3.0});  // Reading is .get(listPosition)[arrayPosition]
+
+
+        System.out.println("B");
+        x = m / seeds[0];
+        while (randomCount != count) {
+
+            int lowestIndex = 0;
+            for (int j = 0; j < events.size(); j++) {
+                if (events.get(lowestIndex)[1] > events.get(j)[1]) {
+                    lowestIndex = j;
+                }
+            }
+
+            if (events.get(lowestIndex)[0] == 0) { // Arrival
+                //System.out.println("Row: " + row + " Capacity: " + capacity);
+                if (row != capacity || capacity == -1) {
+
+                    //System.out.println(row);
+                    row++;
+
+                    if (servers >= row) {
+                        //System.out.println("ATENDIDO");
+                        servers--;
+
+                        nextExit = conversion(minService, maxService, randomizer());
+
+                        time[row] += events.get(lowestIndex)[1] - globalTime;
+                        double v = nextExit + globalTime;
+                        events.add(new double[]{1, v});
+                    }
+
+                } else {
+                    losses++;
+                }
+                //System.out.println("ARRIVAL");
+
+                nextArrival = conversion(minArrival, maxArrival, randomizer());
+                double v = nextArrival + globalTime;
+                globalTime = events.get(lowestIndex)[1];
+                events.add(new double[]{0, nextArrival + globalTime});
+                events.remove(lowestIndex);
+
+                //System.out.println(globalTime);
+
+            } else {
+
+                //System.out.println("Row: " + row + " Capacity: " + capacity);
+                //System.out.println("EXIT");
+
+                time[row] += events.get(lowestIndex)[1] - globalTime;
+                double v = nextExit + globalTime;
+                globalTime = events.get(lowestIndex)[1];
+                events.remove(lowestIndex);
+                row--;
+                servers++;
+                if (row >= servers) {
+                    events.add(new double[]{1, v});
+                }
+            }
+        }
+        System.out.println("C");
+
+        for (double t : time) {
+            System.out.println(t);
+        }
+        System.out.println(losses);
+
+
+
+
+
+
+        /*for (int j = 0; j < count; j++) {
+            // Writes to a .txt file the results of the randomizer method
+            System.out.println(randomizer());
+
+        }*/
     }
+
+    // Method that executes the linear congruential calculation
+    public static double randomizer() {
+        randomCount++;
+        x = (a * x + c) % m;
+
+        return x / m;
+    }
+
+    public static double conversion(double A, double B, double random) {
+        return (B - A) * (random) + A;
+    }
+
 }
